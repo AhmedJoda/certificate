14e418365e20978c17f2594b7c63b923830a6886
ahmed <ahmedjodaalmory99@gmail.com>
2021-03-03 16:35:43 +0200

    add logo

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -3,11 +3,14 @@
 <form action="{{route('settings.store')}}" method="POST" enctype="multipart/form-data">
     @csrf
     <div class="form-group"><label for="">الشعار</label><input type="file" name="logo" class="form-control"></div>
+    @if (setting('logo'))
+    <img src="{{asset('images'.setting('logo'))}}" alt="">
+    @endif
     <div class="form-group"><label for="">اسم الموقع</label><input type="text" name="site_name" value={{setting('site_name')}} class="form-control"></div>
     <div class="form-group"><label for="">ادارة</label><input type="text" name="manger" value={{setting('manger')}} class="form-control"></div>
     <div class="form-group"><label for="">الجوال</label><input type="text" name="phone" value={{setting('phone')}} class="form-control"></div>
     <div class="form-group"><label for="">الرابط</label><input type="text" name="link" value={{setting('link')}} class="form-control"></div>
-    <div class="form-group"><label for="">البريد الالكتروني</label><input type="text" value={{setting('email')}} name="email" class="form-control"></div>
+    <div class="form-group"><label for="">البريد الالكتروني</label><input type="text" value={{setting('email')}}  name="email" class="form-control"></div>
     <div class="form-group"><label for="">تشغيل الموقع</label>
         <input type="hidden" name="active" value="0" />
         <input type="checkbox" name="active" {{setting('active') ? 'checked' : ''}} value="1" class="form-control">
